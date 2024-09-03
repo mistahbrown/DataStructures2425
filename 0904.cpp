52b7ec4d33d0075f5d2765648bbfc69e6c6bb264
zachary.brown <zachary.brown@zacharybrown.local>
2024-09-03 14:45:48 -0400

    okay

---
+++ 0904.cpp
@@ -0,0 +1,44 @@
+#include "0904.h"
+
+int main()
+{
+    cout << "Running pyramid tests..." << endl;
+
+    printPyramid(5);
+    printPyramid(2);
+    printPyramid(8);
+
+    cout << "Running fibonacci tests..." << endl;
+
+    assert(recursiveFibonacci(3) == 2);
+    assert(recursiveFibonacci(0) == 0);
+    assert(recursiveFibonacci(5) == 5);
+    assert(recursiveFibonacci(15) == 610);
+    assert(recursiveFibonacci(-430 == -1));
+
+    cout << "Running palindrome tests..." << endl;
+
+    assert(isPalindrome("racecar"));
+    assert(isPalindrome("tacocat"));
+    assert(isPalindrome("dammit im mad"));
+    assert(!isPalindrome("bruh"));
+    assert(!isPalindrome("Mr. Brown is Awesome"));
+    assert(isPalindrome("a"));
+    assert(isPalindrome(""));
+
+    cout << "Running mode tests..." << endl;
+
+    vector<int> list1 = {};
+    vector<int> list2 = {1, 2, 1};
+    vector<int> list3 = {7};
+    vector<int> list4 = {1, 3, 4, 3, 2, 3, 1, 4, 3, 7, 2};
+    vector<int> list5 = {120, 4, 120, 3, 3, 467, 8, 9, 10, 21};
+    vector<int> list6 = {4, 5, 6, 7, 8};
+
+    assert(mode(list1) == -1);
+    assert(mode(list2) == 1);
+    assert(mode(list3) == 7);
+    assert(mode(list4) == 3);
+    assert(mode(list5) == 120);
+    assert(mode(list6) == 4);
+}
---
+++ 0904.h
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <vector
+using namespace std;
+
+
+/*  printPyramid() - takes in an integer n and prints out n lines of a pyramid
+    formatted like so (up to the line including 128, or n=8): 
+                1
+            1   2   1
+        1   2   4   2   1
+    1   2   4   8   4   2   1
+ */
+
+void printPyramid(int n)
+{
+    // Your code here!
+}
+
+/*  recursiveFibonacci() - takes in an integer n and returns the nth number in the
+    fibonacci sequence using recursion and no iteration! Assume the fibonacci
+    sequence starts with 0 and 1 where n=0 is 0. Since fibonacci is not defined
+    for integers < 0, return -1 if given a negative number.
+*/
+
+int recursiveFibonacci(int n)
+{
+    // Your code here!
+    return 0;
+}
+
+/*  isPalindrome() - takes in a string s and return true if s is a palindrome (same
+    forwards as it is backwards) and false otherwise. Ex: racecar returns true, 
+    chicken returns false. 
+    IMPORTANT: IGNORE/REMOVE SPACES AND PUNCTUATION!
+*/
+
+bool isPalindrome(string s)
+{
+    // Your code here!
+    return false;
+}
+
+/*  mode() - takes in a vector list and returns which number in the list appears
+    most frequently. If multiple elements appear most frequently, return the one that
+    appears first in the list. If list is empty, return -1.
+*/
+
+int mode(vector<int> list)
+{
+    // Your code here!
+    return -1;
+}
